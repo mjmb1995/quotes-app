5cf95a27cd7ba427005ecef361a5be5727492b8b
Mark Joshua Bihag <markjoshuabihag@gmail.com>
2017-11-23 17:44:32 -0800

    completed POST data functionality to create new quotes

--- public/assets/js/quotes.js
+++ public/assets/js/quotes.js
@@ -1,6 +1,26 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
 
-  // jQuery event handlers should go here.
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newQuote = {
+      author: $("#auth").val().trim(),
+      quote: $("#quo").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/quotes", {
+      type: "POST",
+      data: newQuote
+    }).then(
+      function() {
+        console.log("created new quote");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
 
 });
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var bodyParser = require("body-parser");
 var app = express();
 var port = 3000;
 
+// middleware to serve static content for the app
+app.use(express.static("public"));
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -18,7 +21,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "TsUbAsA330",
   database: "quotes_db"
 });
 
@@ -31,6 +34,44 @@ connection.connect(function(err) {
 });
 
 // Express and MySQL code should go here.
+// home page
+app.get("/", function(req, res){
+  connection.query("SELECT * FROM quotes;", function(err, data){
+    if (err){
+      return res.status(500).end();
+    }
+    res.render("index", { quotes: data});
+  });
+});
+
+app.post("/api/quotes", function(req, res){
+  connection.query("INSERT INTO quotes (author, quote) VALUES (?, ?)", [
+    req.body.author, req.body.quote
+  ], function(err, result){
+    if (err) {
+      return res.status(500).end();
+    }
+
+    // Send back the ID of the new wish
+    res.json({ id: result.insertId });
+    console.log({ id: result.insertId });
+  })
+})
+
+// Retrieve all quotes
+app.get("/quotes", function(req, res) {
+  connection.query("SELECT * FROM quotes;", function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.json(data);
+  });
+});
+
+
+
+
 
 app.listen(port, function() {
   console.log("Listening on PORT " + port);
